aaaf825ab8843d4d0caba6a29a61312fb53a78de
Fernanda Andreia <fernanda.nascimento.btk@gmail.com>
2019-02-19 20:51:59 -0300

    Calcular valores e exibir

---
+++ variavel.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*_
+
+valor1 = int(input ("Digite um número aleatório: "))
+valor2 = int(input("Digite outro número aleatório: "))
+soma = valor1+valor2
+subtracao = valor1-valor2
+divisao = valor1/valor2
+resto_da_divisao = valor1%valor2
+media = valor1+valor2/2
+multiplicacao = valor1*valor2
+
+print(f' Os resultados foram: Soma {soma} | Subtracao: {subtracao} | Divisão: {divisao} | Resto da Divisão: {resto_da_divisao} | Média: {media} | Multiplicacao: {multiplicacao}')
